1dbdbc23cfe2c902660f28675a972cc18d8d3f89
LeonardoBringel <leo.sbringel@gmail.com>
2022-06-15 17:27:54 -0300

    :sparkles: Implement transaction class

---
+++ transaction.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+class Transaction:
+    def __init__(self, date, category, order, ticker, quantity, price):
+        temp_date = datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S')
+
+        self.date = temp_date.strftime('%Y-%m-%d')
+        self.category = category
+        self.order = order
+        self.ticker = ticker
+        self.quantity = quantity
+        self.price = round(float(price), 2)
+
+    def __str__(self):
+        return f'{self.date}\t{self.category}\t{self.order}\t' + \
+            f'{self.ticker}\t{self.quantity}\t{self.price}'
