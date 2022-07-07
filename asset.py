69a150326050d5489252659fa1187a758ab87104
LeonardoBringel <leo.sbringel@gmail.com>
2022-07-07 20:45:50 -0300

    :sparkles: Implement asset class

---
+++ asset.py
@@ -0,0 +1,16 @@
+class Asset:
+    def __init__(self, transaction):
+        self.ticker = transaction.ticker
+        self.category = transaction.category
+        self.quantity = transaction.quantity
+        self.total_price = transaction.total_price
+
+    def add_transaction(self, transaction):
+        self.quantity += transaction.quantity
+        self.total_price = round(self.total_price + transaction.total_price, 2)
+
+    def get_average_price(self):
+        return round(self.total_price / self.quantity, 2)
+
+    def __str__(self):
+        return f'{self.category}\t{self.ticker}\t{self.quantity}\t{self.total_price}\t{self.get_average_price()}'
