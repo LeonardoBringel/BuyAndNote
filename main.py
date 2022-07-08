e8b14002776da1fc8105c10e1b74a1570053db24
LeonardoBringel <leo.sbringel@gmail.com>
2022-07-07 21:15:33 -0300

    :lipstick: Implement simple menu to improve usability

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ except FileNotFoundError:
     create_workbook()
     workbook, worksheet = open_workbook()
 
-if input('Add transaction? <y/n> ').upper() == 'Y':
-    add_transaction(worksheet)
-
-for transaction in list_transactions(worksheet):
-    print(transaction)
-
-save_workbook(workbook)
+while True:
+    print('------------- MENU -------------')
+    answer = input('[1] - Inserir\t\t[2] - Visualizar\n[0] - Sair\tOpção: ')
+    match answer:
+        case '1':
+            add_transaction(worksheet)
+            save_workbook(workbook)
+        case '2':
+            print('\n\n-- Transações --')
+            for transaction in list_transactions(worksheet):
+                print(transaction)
+        case '0':
+            exit()
+    print('\n\n\n')
